398c95a6bab349b1bf38a76bfd439be52b36786a
AliQasimzade <qasimzadeali4@gmail.com>
2023-07-27 16:40:56 +0400

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,71 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import React, { useState, useEffect } from "react";
 
-export default function Home() {
+import axios from "axios";
+import Products from "@/components/Products/Products";
+import Filters from "@/components/Filters/Filters";
+
+const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [filterProducts, setFilterProducts] = useState([]);
+  const [brands, setBrands] = useState<string[]>([]);
+  const [ratings, setRatings] = useState<number[]>([]);
+  const [prices, setPrices] = useState<number[]>([]);
+
+  const getProducts = async () => {
+    try {
+      const request = await axios.get("https://dummyjson.com/products");
+
+      if (request.status !== 200) {
+        throw new Error("Xeta bas verdi");
+      } else {
+        setProducts(request.data.products);
+        const fullBrands: string[] = request.data.products.map(
+          (product: any) => product.brand
+        );
+
+        const fullRatings: number[] = request.data.products.map(
+          (product: any) => product.rating
+        );
+
+        const fullPrices: number[] = request.data.products.map(
+          (product: any) => product.price
+        );
+        const uniqueBrandsSet: Set<string> = new Set<string>(fullBrands);
+        const uniqueRatingSet: Set<number> = new Set<number>(fullRatings);
+        const uniquePriceSet: Set<number> = new Set<number>(fullPrices);
+        const myBrands: string[] = Array.from(uniqueBrandsSet);
+        const myRatings: number[] = Array.from(uniqueRatingSet);
+        const myPrices: number[] = Array.from(uniquePriceSet);
+        console.log(myBrands);
+        setBrands(myBrands);
+        setRatings(myRatings);
+        setPrices(myPrices);
+
+        setFilterProducts(request.data.products);
+      }
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <>
+      {products.length > 0 && (
+        <Filters
+          brands={brands}    
+          prices={prices}
+          ratings={ratings}
+          setFilterProducts={setFilterProducts}
+          products={products}
         />
-      </div>
+      )}
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      {products.length > 0 && <Products products={filterProducts} />}
+    </>
+  );
+};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
